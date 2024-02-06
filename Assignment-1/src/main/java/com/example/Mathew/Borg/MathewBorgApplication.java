612a01bfa0a59b5eddcc73d7b129c85260971187
Mat4539 <mborg0020@gmail.com>
2024-02-05 21:19:09 -0500

    Inital Commit

---
+++ Assignment-1/src/main/java/com/example/Mathew/Borg/MathewBorgApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mathew.Borg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathewBorgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathewBorgApplication.class, args);
+	}
+
+}
---
+++ Assignment-1/src/test/java/com/example/Mathew/Borg/MathewBorgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mathew.Borg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathewBorgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
